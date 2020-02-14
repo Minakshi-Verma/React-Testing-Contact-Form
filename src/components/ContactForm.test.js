4e87a4e9cabdddece6d7da1856cb64043156622f
Minakshi Verma <mvedwan@gmail.com>
2020-02-13 20:08:04 -0500

    "testing"

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('firstName, lastName, email, message inputs are rendered',() =>{
@@ -10,16 +10,48 @@ test('firstName, lastName, email, message inputs are rendered',() =>{
     getByLabelText(/message/i);
 });
 
+test ('form submit adds new user to the list', ()=>{
+const{ getByLabelText } = render(<ContactForm />);
+const firstNameInput = getByLabelText(/firstName/i);
+const lastNameInput = getByLabelText(/lastName/i);
+const emailInput = getByLabelText(/email/i);
+const messageInput = getByLabelText(/message/i);
+
+fireEvent.change(firstNameInput, {target:{value: 'test firstName'}})
+
+fireEvent.change(lastNameInput, {target:{value: 'test lastName'}})
+
+fireEvent.change(emailInput, {target:{value: 'test email'}})
+
+fireEvent.change(messageInput, {target:{value: 'test message'}})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // test('displays error message when missing required param'() =>
 // )
 
-test('firstName, lastName, email is rendered',()=>{
-    const{ getByPlaceholderText} = render(<ContactForm />)
-    getByPlaceholderText(/bill/i)
-    getByPlaceholderText(/luo/i)
-    getByPlaceholderText(/bluebill1049@hotmail.com/i)
-})
+// test('firstName, lastName, email is rendered',()=>{
+//     const{ getByPlaceholderText} = render(<ContactForm />)
+//     getByPlaceholderText(/bill/i)
+//     getByPlaceholderText(/luo/i)
+//     getByPlaceholderText(/bluebill1049@hotmail.com/i)
+// })
 
 
 
